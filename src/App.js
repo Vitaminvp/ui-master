b044254b1a4febaaa93187301a1d9ff6de1fb063
Vitalii Vitaminenko <vitamin@ukr.net>
2019-06-06 23:10:49 +0300

    Add chart

--- src/App.js
+++ src/App.js
@@ -1,34 +1,125 @@
 import React, { Component } from "react";
 import pigData from "./wild-pig-data.json";
+import { Chart } from "./Chart";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super(null);
+    this.state = {
+      chartData: {}
+    };
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: [
+          "Boston",
+          "Worcester",
+          "Springfield",
+          "Lowell",
+          "Cambridge",
+          "New Bedford"
+        ],
+        datasets: [
+          {
+            label: "Population",
+            data: [617594, 181045, 153060, 106519, 105162, 95072],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <table>
-          <tbody>
-            <tr>
-              <th>Year</th>
-              <th>Island</th>
-              <th>Population</th>
-            </tr>
-            {pigData["PIG POPULATIONS"].map((datum, index) => (
-              <tr key={index}>
-                <td>{datum.year}</td>
-                <td>{datum.island}</td>
-                <td>{datum.pigPopulation}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <Chart
+          chartData={this.state.chartData}
+          location="Massachusetts"
+          legendPosition="bottom"
+        />
       </div>
     );
   }
 }
 
 export default App;
+{
+  /*<div className="App">*/
+}
+{
+  /*<p className="App-intro">*/
+}
+{
+  /*To get started, edit <code>src/App.js</code> and save to reload.*/
+}
+{
+  /*</p>*/
+}
+
+{
+  /*<table>*/
+}
+{
+  /*<tbody>*/
+}
+{
+  /*<tr>*/
+}
+{
+  /*<th>Year</th>*/
+}
+{
+  /*<th>Island</th>*/
+}
+{
+  /*<th>Population</th>*/
+}
+{
+  /*</tr>*/
+}
+{
+  /*{pigData["PIG POPULATIONS"].map((datum, index) => (*/
+}
+{
+  /*<tr key={index}>*/
+}
+{
+  /*<td>{datum.year}</td>*/
+}
+{
+  /*<td>{datum.island}</td>*/
+}
+{
+  /*<td>{datum.pigPopulation}</td>*/
+}
+{
+  /*</tr>*/
+}
+{
+  /*))}*/
+}
+{
+  /*</tbody>*/
+}
+{
+  /*</table>*/
+}
+{
+  /*</div>*/
+}
---
+++ src/Chart/Chart.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
+
+class Chart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData
+    };
+  }
+
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: "right",
+    location: "City"
+  };
+
+  render() {
+      const {displayTitle, location, displayLegend ,legendPosition} = this.props;
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: displayTitle,
+              text: "Pig population in " + location,
+              fontSize: 25
+            },
+            legend: {
+              display: displayLegend,
+              position: legendPosition
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default Chart;
